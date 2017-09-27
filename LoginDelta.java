9bf215a17c5fd8897accea3ddd37ae5d466b52b8
Tarun Reddy <tarunreddy@Taruns-MacBook-Air.local>
2017-09-26 17:07:54 -0700

    Initial commit

---
+++ LoginDelta.java
@@ -0,0 +1,11 @@
+package DeltaPro;
+
+public class LoginDelta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("true");
+System.out.println("hello world");
+	}
+
+}
